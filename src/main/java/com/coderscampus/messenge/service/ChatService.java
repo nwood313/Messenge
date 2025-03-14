71c8259ef5e09b82c3da75e66674de1725e8e0b3
nwood313 <nwood313@gmail.com>
2025-03-13 22:07:46 -0400

    Added Channel Service logic and modified some Repository work. More to come

--- src/main/java/com/coderscampus/messenge/service/ChatService.java
+++ src/main/java/com/coderscampus/messenge/service/ChatService.java
@@ -1,4 +1,30 @@
 package com.coderscampus.messenge.service;
 
+package com.coderscampus.messenge.service;
+
+import com.coderscampus.messenge.dto.Chat;
+import com.coderscampus.messenge.repository.ChatRepository;
+import com.coderscampus.messenge.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ChatService {
+    private final UserRepository userRepository;
+    private final ChatRepository chatRepository;
+
+    public ChatService(UserRepository userRepo,
+                       ChatRepository chatRepo) {
+        this.userRepository = userRepo;
+        this.chatRepository = chatRepo;
+    }
+
+    public void sendChats(Chat chat) {
+        chatRepo.save(chat);
+    }
+
+    public List<Chat> getChats(Long channelId) {
+        return chatRepo.findByChannelId(channelId);
+    }
 }
